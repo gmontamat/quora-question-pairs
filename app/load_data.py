647a030a691212faa30816ba6f8946f924cfef37
Gustavo Montamat <gmontamat@gmail.com>
2017-04-10 22:44:24 -0300

    Remove unnecessary imports

--- app/load_data.py
+++ app/load_data.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
 """
-Load CSV files
+Load data files
 """
 
-import os
 import pandas as pd
 
 
-def read_csv(file_path):
-    return pd.read_csv(file_path, sep=',')
+def load_data(file_path):
+    return pd.read_csv(file_path)
 
 
 if __name__ == '__main__':
-    print read_csv('../data/test.csv')
+    train = load_data('../data/train.csv')
+    print list(train)
