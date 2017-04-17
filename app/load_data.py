518be5e710c5fe169a920b4aaa33470120b57877
Gustavo Montamat <gmontamat@gmail.com>
2017-04-16 23:51:26 -0300

    Remove unnecessary columns from dataframe

--- app/load_data.py
+++ app/load_data.py
@@ -8,7 +8,13 @@ import pandas as pd
 
 
 def load_data(file_path):
-    return pd.read_csv(file_path)
+    df = pd.read_csv(file_path)
+    try:
+        # Unnecessary fields in training data
+        df = df.drop(['id', 'qid1', 'qid2'], axis=1)
+    except ValueError:
+        pass
+    return df
 
 
 if __name__ == '__main__':
