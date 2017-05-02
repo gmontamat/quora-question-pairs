2f263351d2936a5944b82b1a9058cd2d00eb2b6a
Gustavo Montamat <gmontamat@gmail.com>
2017-05-01 23:13:20 -0300

    Implement Wikipedia-based corrector

--- app/spell_checker.py
+++ app/spell_checker.py
@@ -4,9 +4,11 @@
 Spell checks text
 """
 
-import collections
+import os
 import re
 import string
+
+from collections import Counter
 
 
 class SpellChecker(object):
@@ -16,19 +18,19 @@ class SpellChecker(object):
 
     def __init__(self, words_file):
         words = self.get_words(open(words_file).read())
-        self.dictionary = collections.Counter(words)
+        self.dictionary = Counter(words)
         self.n_words = float(sum(self.dictionary.values()))
 
     @staticmethod
     def get_words(text):
-        """Find words in text file
+        """Get words from 'text'
         """
         return re.findall(r'\w+', text.lower())
 
     def double_edit(self, word):
         """Get set of words that are two edits away from 'word'
         """
-        return set(e2 for e1 in self.single_edit(word) for e2 in self.single_edit(e1))
+        return (e2 for e1 in self.single_edit(word) for e2 in self.single_edit(e1))
 
     @staticmethod
     def single_edit(word):
@@ -59,7 +61,7 @@ class SpellChecker(object):
             self.known_words([word]) or
             self.known_words(self.single_edit(word)) or
             self.known_words(self.double_edit(word)) or
-            set([word])
+            [word]
         )
         return candidates
 
@@ -68,8 +70,77 @@ class SpellChecker(object):
         """
         return max(self.get_candidates(word), key=self.word_probability)
 
+    def clean_questions(self, question1, question2):
+        """Correct misspellings in a question using a possible matching one
+        """
+        words1 = self.get_words(question1)
+        words2 = self.get_words(question2)
+        checked_words1 = []
+        checked_words2 = []
+        for word in words1:
+            if word in self.dictionary or word in words2:
+                checked_words1.append(word)
+                continue
+            for candidate in self.get_candidates(word):
+                if candidate in words2:
+                    checked_words1.append(candidate)
+                    print '{} ==> {}'.format(word, candidate)
+                    break
+            else:
+                correction = self.correct(word)
+                print '{} ==> {}'.format(word, correction)
+                checked_words1.append(correction)
+        for word in words2:
+            if word in self.dictionary or word in words1:
+                checked_words2.append(word)
+                continue
+            for candidate in self.get_candidates(word):
+                if candidate in words1:
+                    checked_words2.append(candidate)
+                    print '{} ==> {}'.format(word, candidate)
+                    break
+            else:
+                correction = self.correct(word)
+                print '{} ==> {}'.format(word, correction)
+                checked_words2.append(correction)
+        return ' '.join(checked_words1), ' '.join(checked_words2)
+
+
+class WikiSpellChecker(SpellChecker):
+    """Spell checker that obtains vocabulary from a Wikipedia dump
+    """
+
+    def __init__(self, wiki_path):
+        self.dictionary = self.load_wiki_dump(wiki_path)
+        self.n_words = float(sum(self.dictionary.values()))
+
+    def load_wiki_dump(self, wiki_path):
+        dictionary = Counter()
+        wiki_files = [
+            os.path.join(path, file_name)
+            for path, subdir, files in os.walk(wiki_path)
+            for file_name in files if 'wiki_' in file_name
+        ]
+        for wiki_file in wiki_files:
+            text = open(wiki_file).read()
+            pattern = re.compile(r'<.*?>')
+            text = pattern.sub('', text)
+            dictionary.update(self.get_words(text))
+        return dictionary
+
 
 if __name__ == '__main__':
-    sc = SpellChecker('../dictionaries/big.txt')
-    print sc.get_candidates('whatsover')
+    # sc = SpellChecker('../dictionaries/big.txt')
+    sc = WikiSpellChecker('../dictionaries/text/')
     print sc.correct('whatsover')
+    print sc.correct('verfify')
+    print sc.correct('nintendo')
+
+    from load_data import load_data
+    from cleaner import DataCleaner
+    train = load_data('../data/train.csv')
+    dc = DataCleaner(train)
+    dc.clean_column('question1')
+    dc.clean_column('question2')
+    for question1, question2 in zip(train.question1, train.question2):
+        sc.clean_questions(question1, question2)
