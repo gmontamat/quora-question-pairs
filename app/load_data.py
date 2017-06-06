9acb758268047825e4f9f8a0c5e49b1248b74143
Gustavo Montamat <gmontamat@gmail.com>
2017-06-06 01:11:15 -0300

    Simplify id columns removal

--- app/load_data.py
+++ app/load_data.py
@@ -10,8 +10,7 @@ import pandas as pd
 def load_data(file_path, remove_id_columns=True):
     df = pd.read_csv(file_path)
     if remove_id_columns:
-        remove_columns = [column for column in ['id', 'qid1', 'qid2'] if column in df]
-        df = df.drop(remove_columns, axis=1)
+        return df.drop([column for column in ['id', 'qid1', 'qid2'] if column in df], axis=1)
     return df
 
 
